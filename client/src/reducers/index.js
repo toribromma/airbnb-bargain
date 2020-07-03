fd03e48021e0afa85c7619118b64857692bc884e
Tori <torimpham@gmail.com>
2020-07-03 13:15:19 -0700

    added results reducer to index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducers";
 import errorReducer from "./errorReducers";
+import resultsReducer from "./resultsReducers"
 export default combineReducers({
   auth: authReducer,
-  errors: errorReducer
+  errors: errorReducer,
+  search: resultsReducer
 });
