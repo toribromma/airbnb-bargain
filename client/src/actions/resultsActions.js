24114bcde087f7f5edaa61f7170cad5b4379f160
Tori <torimpham@gmail.com>
2020-07-03 12:12:52 -0700

    added fetch function to grab data from api call

--- client/src/actions/resultsActions.js
+++ client/src/actions/resultsActions.js
@@ -4,6 +4,30 @@ import {
     FETCH_RESULTS_ERROR
   } from "./types";
 
+  export const fetchProducts = data => {
+    return dispatch => {
+        dispatch(fetchProductsPending());
+        fetch("https://mashvisor-api.p.rapidapi.com/airbnb-property/active-listings?zip_code=91342&page=1&city=San%20Francisco&items=10&state=CA", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "mashvisor-api.p.rapidapi.com",
+                "x-rapidapi-key": `${process.env.REACT_APP_MV_KEY}`
+            }
+        })
+        .then(res => res.json())
+        .then(res => {
+            if(res.error) {
+                throw(res.error);
+            }
+            dispatch(fetchProductsSuccess(res.products);
+            return res.products;
+        })
+        .catch(error => {
+            dispatch(fetchProductsError(error));
+        })
+    }
+}
+
 export const fetchResultsPending = () => {
     return {
         type: FETCH_RESULTS_PENDING
