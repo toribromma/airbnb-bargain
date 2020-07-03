542cb867d2b6ec2e6e21a20331310264f7b5736d
Tori <torimpham@gmail.com>
2020-07-03 10:13:38 -0700

    added action creators for api call

---
+++ client/src/actions/resultsActions.js
@@ -0,0 +1,25 @@
+import {
+    FETCH_RESULTS_PENDING,
+    FETCH_RESULTS_SUCCESS,
+    FETCH_RESULTS_ERROR
+  } from "./types";
+
+export const fetchResultsPending = () => {
+    return {
+        type: FETCH_RESULTS_PENDING
+    };
+};
+
+export const fetchResultsSuccess = results => {
+    return {
+        type: FETCH_RESULTS_SUCCESS
+        payload: results
+    };
+};
+
+export const fetchResultsError = error => {
+    return {
+        type: FETCH_RESULTS_ERROR
+        payload: error
+    }
+}
