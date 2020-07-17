de761114c5c7aa12a27e6a40db4fb15545321ae4
toritheterrible <torimpham@gmail.com>
2020-07-17 10:09:02 -0700

    changes to server.js

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.use(bodyParser.json({ type: 'application/*+json' }))
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 // DB Config
@@ -39,10 +42,6 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 app.use("/api/mashvisor", mashvisor);
-
-app.use(function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
