45f5671ec886b8839d049955e0bf2d98089ad0e4
toritheterrible <torimpham@gmail.com>
2020-07-17 09:53:46 -0700

    changes to server.js file

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const app = express();
-const routes = require("./routes");
 const users = require("./routes/api/users")
 const mashvisor = require("./routes/api/mashvisor")
-
+const path = require("path")
 
 
 // Bodyparser middleare
@@ -38,12 +37,12 @@ app.use(passport.initialize());
 // Passport config
 require("./config/passport")(passport);
 // Routes
-app.use(routes);
 app.use("/api/users", users);
 app.use("/api/mashvisor", mashvisor);
 
-
-
+app.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
