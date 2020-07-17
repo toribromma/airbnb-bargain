b84147c139627da6e2b47e067de88426cac7ce32
toritheterrible <torimpham@gmail.com>
2020-07-17 09:15:03 -0700

    reverted server.js to original

--- server.js
+++ server.js
@@ -16,11 +16,6 @@ app.use(
 );
 app.use(bodyParser.json({ type: 'application/*+json' }))
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add 
-
 // DB Config
 const db = require("./config/keys").mongoURI;
 // Connect to MongoDB
